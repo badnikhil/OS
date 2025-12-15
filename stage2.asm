692cc9fb107167ec38b606c2acf8b04700bbfa50
Nikhil <nikhilljatt@gmail.com>
2025-12-15 16:46:22 +0530

    just finishing

--- stage2.asm
+++ stage2.asm
@@ -6,18 +6,6 @@
     call update_cursor
     mov esi , msg
     call print_string
-    jmp $
-    mov edi, 0xB8000
-    mov ax, 0x0720
-    mov [edi], ax
-    add edi, 2
-    mov [edi], ax
-    add edi, 2
-    mov [edi], ax
-    add edi, 2
-    mov [edi], ax
-    add edi, 2
-    mov [edi], ax
     ;Stable 32 bit mode in CPU
     jmp $
 
